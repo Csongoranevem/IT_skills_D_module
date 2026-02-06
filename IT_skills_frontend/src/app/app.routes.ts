13a7bd55cf8b0959b2b50c4476f491e1326ae605
PiciMadar <rekdavid@turr.hu>
2026-02-06 10:09:57 +0100

    Carousel behúzása

--- IT_skills_frontend/src/app/app.routes.ts
+++ IT_skills_frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './user/login/login.component';
+import { HomeComponent } from './system/home/home.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'home', component:HomeComponent},
     { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
