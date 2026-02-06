91ce1e814139ff94a149db3725653caf31bf8b90
Zalan12 <farkaszalan@turr.hu>
2026-02-06 11:25:49 +0100

    Footer contents added

--- IT_skills_frontend/src/app/system/footer/footer.component.ts
+++ IT_skills_frontend/src/app/system/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
 
+  footerText:string[]=["2","0","2","6",","," ","M","i","n","d","e","n"," j","o","g"," f","e","n","n","t","a","r","t","v","a"];
+  ngOnInit(): void {
+      this.getFooterText();
+  }
+
+   getFooterText(){
+    let FText:any=document.getElementById("footerText");
+    for (let i = 0; i < this.footerText.length; i++) {
+      setTimeout(() => {
+        FText.innerText+=this.footerText[i];
+      }, 1);
+      
+    }
+  }
 }
