0712e684d7756d761f9962e255713be97c8d2031
Csongoranevem <czakocsongor@turr.hu>
2026-02-13 11:35:12 +0100

    message és bejelentkezés

--- IT_skills_frontend/src/app/user/login/login.component.ts
+++ IT_skills_frontend/src/app/user/login/login.component.ts
@@ -1,20 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { APIService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { MessageService } from '../../services/message.service';
+import { Router } from '@angular/router';
+import { MessageComponent } from '../../system/message/message.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, MessageComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  rememberMe: boolean = false;
 
   ngOnInit(): void {
   }
 
-  login(): void {
-  }
+  constructor(
+    private api: APIService,
+    private auth: AuthService,
+    private message: MessageService,
+    private router: Router
 
+  ) { }
+
+
+
+  login() {
+
+    if (!this.email || !this.password) {
+      this.message.show('warning', 'Figyelem', 'Kérlek töltsd ki az összes mezőt!');
+      return;
+    }
+    this.api.Login(this.email, this.password).then(res => {
+      if (res.status == 500) {
+        this.message.show('danger', 'Hiba', res.message!);
+        return
+      }
+
+      if (this.rememberMe) {
+        this.auth.storeUser(JSON.stringify(res.data))
+      }
+
+      this.auth.login(JSON.stringify(res.data));
+      this.router.navigate(['/home']);
+      this.message.show('success', '', "Sikeresen bejelentkeztél!");
+    })
+  }
 }
